f416843538f9dc22e96f19c514d2b1a211749b8e
Adrian Chadd <adrian@freebsd.org>
2015-10-04 10:40:18 -0700

    Start fleshing out some split-brain portability stuff along the lines of the ath(4) driver.
    Bring over a couple of initial headers from ath10k into HAL.
    Add a simple linux typedef thing for the base types.
    
    I'm hoping that I can keep some semblence of OS abstraction here;
    enough to make future porting less terrible.

---
+++ otus/freebsd/src/sys/dev/athp/hal/linux_compat.h
@@ -0,0 +1,13 @@
+#ifndef	__LINUX_COMPAT_H__
+#define	__LINUX_COMPAT_H__
+
+typedef uint8_t		u8;
+typedef uint16_t	u16;
+typedef uint32_t	u32;
+typedef uint64_t	u64;
+
+typedef uint16_t	__le16;
+typedef uint32_t	__le32;
+typedef uint64_t	__le64;
+
+#endif	/* __LINUX_COMPAT_H__ */
