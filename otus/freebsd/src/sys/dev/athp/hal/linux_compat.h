f4d4274e1449279bd381fe7b2ed73068c45e7877
Adrian Chadd <adrian@freebsd.org>
2015-10-15 23:50:28 -0700

    Bits needed for wmi.h

--- otus/freebsd/src/sys/dev/athp/hal/linux_compat.h
+++ otus/freebsd/src/sys/dev/athp/hal/linux_compat.h
@@ -8,9 +8,18 @@ typedef uint16_t	u16;
 typedef uint32_t	u32;
 typedef uint64_t	u64;
 
+typedef int8_t		s8;
+typedef int16_t		s16;
+typedef int32_t		s32;
+typedef int64_t		s64;
+
 typedef uint16_t	__le16;
 typedef uint32_t	__le32;
 typedef uint64_t	__le64;
+
+typedef uint16_t	__be16;
+typedef uint32_t	__be32;
+typedef uint64_t	__be64;
 
 #define	__cpu_to_le32(a)	htole32(a)
 #define	__cpu_to_le16(a)	htole16(a)
@@ -46,9 +55,15 @@ roundup_pow_of_two(unsigned long n)
 
 #define	DECLARE_BITMAP(n, s)	bitstr_t bit_decl(n, s)
 #define	test_bit(i, n)		bit_test(n, i)
-#define	set_bit(i, n)		bit_set(n, i)
+#define	__set_bit(i, n)		bit_set(n, i)
 #define	clear_bit(i, n)		bit_clear(n, i)
 
+#define	set_bit(i, n)		__set_bit(i, n)
+
 #define	min_t(t, a, b)		MIN(a, b)
+
+#define	BIT(x)			(1 << (x))
+
+#define	ETH_ALEN		ETHER_ADDR_LEN
 
 #endif	/* __LINUX_COMPAT_H__ */
