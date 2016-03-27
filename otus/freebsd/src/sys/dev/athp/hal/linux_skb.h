d96eb3118983d611541fde05241c84a1b2203df4
Adrian Chadd <adrian@freebsd.org>
2016-03-27 00:37:22 -0700

    Add a method for skb_put()

--- otus/freebsd/src/sys/dev/athp/hal/linux_skb.h
+++ otus/freebsd/src/sys/dev/athp/hal/linux_skb.h
@@ -50,4 +50,25 @@ mbuf_skb_trim(struct mbuf *m, int len)
 	m->m_pkthdr.len = len;
 }
 
+/*
+ */
+static inline char *
+mbuf_skb_put(struct mbuf *m, int len)
+{
+	char *s;
+
+	/* Get pointer to the end of the buf */
+	s = m->m_data + m->m_len;
+
+	/* Ok, good, now append */
+	m->m_len += len;
+	m->m_pkthdr.len += len;
+
+	/*
+	 * Return pointer to where the end was so we can
+	 * populate it with data.
+	 */
+	return s;
+}
+
 #endif	/* __LINUX_COMPAT_SKB_H__ */
