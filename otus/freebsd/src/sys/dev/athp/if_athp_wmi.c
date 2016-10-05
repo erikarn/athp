fb18e8434967df982cacb20accb7583e2edb4d8a
Adrian Chadd <adrian@freebsd.org>
2016-10-04 22:27:47 -0700

    Tidy up some more of the debug stuff.

--- otus/freebsd/src/sys/dev/athp/if_athp_wmi.c
+++ otus/freebsd/src/sys/dev/athp/if_athp_wmi.c
@@ -1831,16 +1831,22 @@ int ath10k_wmi_cmd_send(struct ath10k *ar, struct athp_buf *pbuf, u32 cmd_id)
 	interval = ticks + ((3000 * hz) / 1000);
 
 	/*
+	 * Just to generate the debug echo; when it's implemented, uncomment
+	 * the one below.
+	 */
+	ath10k_wmi_tx_beacons_nowait(ar);
+
+	/*
 	 * XXX TODO: this is in milliseconds, which likely needs to be more
 	 * frequent for this kind of thing.
 	 */
-//	ath10k_dbg(ar, ATH10K_DBG_WMI, "%s: setup: cmdid=%u, ticks=%u, interval=%u\n", __func__, cmd_id, ticks, interval);
+	ath10k_dbg(ar, ATH10K_DBG_WMI, "%s: setup: cmdid=0x%08x, ticks=%u, interval=%u\n", __func__, cmd_id, ticks, interval);
 	while (! ieee80211_time_after(ticks, interval)) {
 //		ath10k_dbg(ar, ATH10K_DBG_WMI, "%s: loop: cmdid=%u, ticks=%u, interval=%u\n", __func__, cmd_id, ticks, interval);
 		ath10k_wait_wait(&ar->wmi.tx_credits_wq, "tx_credits_wq", 1);
 
 		/* try to send pending beacons first. they take priority */
-		ath10k_wmi_tx_beacons_nowait(ar);
+//		ath10k_wmi_tx_beacons_nowait(ar);
 
 		/* Try to send something */
 		ret = ath10k_wmi_cmd_send_nowait(ar, pbuf, cmd_id);
