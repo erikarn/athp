3c9e48484b25cc08e73f5206c7f7d792d06c997b
Adrian Chadd <adrian@FreeBSD.org>
2025-06-11 12:53:06 -0700

    [mtwn] add bare bones USB attach/detach driver for the MT7610 USB
    This just attaches/detaches from my tplink Archer T2UH NIC.
    It's missing .. well, everything.

---
+++ otus/freebsd/src/sys/dev/mtwn/if_mtwn.c
@@ -0,0 +1,182 @@
+/*-
+ * Copyright 2025 Adrian Chadd <adrian@FreeBSD.org>.
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include "opt_wlan.h"
+
+#include <sys/param.h>
+#include <sys/systm.h>
+#include <sys/bus.h>
+#include <sys/endian.h>
+#include <sys/eventhandler.h>
+#include <sys/firmware.h>
+#include <sys/kdb.h>
+#include <sys/kernel.h>
+#include <sys/linker.h>
+#include <sys/lock.h>
+#include <sys/malloc.h>
+#include <sys/mbuf.h>
+#include <sys/module.h>
+#include <sys/mutex.h>
+#include <sys/socket.h>
+#include <sys/sockio.h>
+#include <sys/sysctl.h>
+
+#include <net/bpf.h>
+#include <net/ethernet.h>
+#include <net/if.h>
+#include <net/if_arp.h>
+#include <net/if_dl.h>
+#include <net/if_media.h>
+#include <net/if_types.h>
+#include <net/if_var.h>
+#include <net80211/ieee80211_var.h>
+#include <net80211/ieee80211_radiotap.h>
+#include <net80211/ieee80211_ratectl.h>
+#include <net80211/ieee80211_regdomain.h>
+#ifdef	IEEE80211_SUPPORT_SUPERG
+#include <net80211/ieee80211_superg.h>
+#endif
+#include <netinet/if_ether.h>
+#include <netinet/in.h>
+#include <netinet/in_systm.h>
+#include <netinet/in_var.h>
+#include <netinet/ip.h>
+
+#include <dev/usb/usb.h>
+#include <dev/usb/usbdi.h>
+
+#include "usbdevs.h"
+
+#include <dev/usb/usb_debug.h>
+#include <dev/usb/usb_msctest.h>
+
+#include "if_mtwn_var.h"
+#include "if_mtwn_debug.h"
+
+static const STRUCT_USB_HOST_ID mtwn_devs[] = {
+#define MTWN_DEV(v, p)                                        \
+	{                                                     \
+		USB_VP(USB_VENDOR_##v, USB_PRODUCT_##v##_##p) \
+	}
+	{ USB_VP(0x148f, 0x761a) },
+};
+#undef MTWN_DEV
+
+static eventhandler_tag mtwn_etag;
+
+static device_probe_t mtwn_match;
+static device_attach_t mtwn_attach;
+static device_detach_t mtwn_detach;
+
+static int
+mtwn_match(device_t self)
+{
+	struct usb_attach_arg *uaa = device_get_ivars(self);
+
+	if (uaa->usb_mode != USB_MODE_HOST)
+		return (ENXIO);
+	if (uaa->info.bConfigIndex != 0)
+		return (ENXIO);
+	if (uaa->info.bIfaceIndex != 0)
+		return (ENXIO);
+
+	return (usbd_lookup_id_by_uaa(mtwn_devs, sizeof(mtwn_devs), uaa));
+}
+
+static int
+mtwn_attach(device_t self)
+{
+	struct mtwn_softc *sc = device_get_softc(self);
+	struct usb_attach_arg *uaa = device_get_ivars(self);
+
+	device_set_usb_desc(self);
+	sc->sc_udev = uaa->device;
+	sc->sc_dev = self;
+
+	mtx_init(&sc->sc_mtx, device_get_nameunit(sc->sc_dev),
+	    MTX_NETWORK_LOCK, MTX_DEF);
+
+	device_printf(sc->sc_dev, "%s: hi!\n", __func__);
+
+	return (0);
+}
+
+static int
+mtwn_detach(device_t self)
+{
+	struct mtwn_softc *sc = device_get_softc(self);
+
+	sc->sc_detached = 1;
+
+	return (0);
+}
+
+static void
+mtwn_autoinst(void *arg, struct usb_device *udev, struct usb_attach_arg *uaa)
+{
+	struct usb_interface *iface;
+	struct usb_interface_descriptor *id;
+
+	if (uaa->dev_state != UAA_DEV_READY)
+		return;
+
+	iface = usbd_get_iface(udev, 0);
+	if (iface == NULL)
+		return;
+	id = iface->idesc;
+	if (id == NULL || id->bInterfaceClass != UICLASS_MASS)
+		return;
+	if (usbd_lookup_id_by_uaa(mtwn_devs, sizeof(mtwn_devs), uaa))
+		return;
+
+	if (usb_msc_eject(udev, 0, MSC_EJECT_STOPUNIT) == 0)
+	uaa->dev_state = UAA_DEV_EJECTING;
+}
+
+static int
+mtwn_driver_loaded(struct module *mod, int what, void *arg)
+{
+	switch (what) {
+	case MOD_LOAD:
+		mtwn_etag = EVENTHANDLER_REGISTER(usb_dev_configured,
+		    mtwn_autoinst, NULL, EVENTHANDLER_PRI_ANY);
+		break;
+	case MOD_UNLOAD:
+		EVENTHANDLER_DEREGISTER(usb_dev_configured, mtwn_etag);
+		break;
+	default:
+		return (EOPNOTSUPP);
+	}
+	return (0);
+}
+
+static device_method_t mtwn_methods[] = {
+	/* Device interface */
+	DEVMETHOD(device_probe, mtwn_match),
+	DEVMETHOD(device_attach, mtwn_attach),
+	DEVMETHOD(device_detach, mtwn_detach), DEVMETHOD_END
+};
+
+static driver_t mtwn_driver = { .name = "mtwn",
+	.methods = mtwn_methods,
+	.size = sizeof(struct mtwn_softc) };
+
+DRIVER_MODULE(mtw, uhub, mtwn_driver, mtwn_driver_loaded, NULL);
+MODULE_DEPEND(mtwn, wlan, 1, 1, 1);
+MODULE_DEPEND(mtwn, usb, 1, 1, 1);
+MODULE_DEPEND(mtwn, firmware, 1, 1, 1);
+MODULE_VERSION(mtwn, 1);
+USB_PNP_HOST_INFO(mtwn_devs);
